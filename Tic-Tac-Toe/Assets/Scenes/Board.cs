adc6622f1233b61d6806bd4c86e495ebbd231bd6
etomsky <etomsy@yahoo.com>
2018-10-14 11:07:37 -0400

    Creating Colliders
    Created empty game objects with box colliders

---
+++ Tic-Tac-Toe/Assets/Scenes/Board.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    private void OnMouseEnter()
+    {
+        print("Board:OnMouseEnter()");
+    }
+
+    private void OnMouseExit()
+    {
+        print("Board:OnMouseExit()");
+    }
+}
