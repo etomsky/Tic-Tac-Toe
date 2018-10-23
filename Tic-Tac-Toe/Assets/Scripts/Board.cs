0ceb3f18f16ce02f13d43e8d945bae85d35c6940
etomsky <etomsy@yahoo.com>
2018-10-23 09:51:40 -0400

    Scripts
    Some work on scripts

---
+++ Tic-Tac-Toe/Assets/Scripts/Board.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode
+{
+    idle,
+    playing,
+    levelEnd
+}
+
+public class Board : MonoBehaviour {
+    static private Board S;  // A private Singleton
+
+    [Header("Set in Inspector")]
+    public Text countXText;
+    public Text countOText;
+    public Text winText;
+    public GameObject[] letters; // An array of the letters
+
+    private void OnMouseEnter()
+    {
+        print("Board:OnMouseEnter()");
+    }
+
+    private void OnMouseExit()
+    {
+        print("Board:OnMouseExit()");
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
---
+++ Tic-Tac-Toe/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Text countXText;
+    public Text countOText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int countX;
+    private int countO;
+
+	// Use this for initialization
+	void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        countX = 0;
+        countO = 0;
+        SetCountXText();
+        SetCountOText();
+        countXText.text = "";
+        countOText.text = "";
+        winText.text = "";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("X"))
+        {
+            other.gameObject.SetActive(false);
+            countX = countX + 1;
+            SetCountXText();
+        }
+        if (other.gameObject.CompareTag("O"))
+        {
+            other.gameObject.SetActive(false);
+            countO = countO + 1;
+            SetCountOText();
+        }
+    }
+
+    private void SetCountXText()
+    {
+        countXText.text = "X Count: " + countX.ToString();
+        if (countX == 3)
+        {
+            winText.text = "You win!";
+        }
+    }
+
+    private void SetCountOText()
+    {
+        countOText.text = "O Count: " + countO.ToString();
+        if (countO == 3)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
